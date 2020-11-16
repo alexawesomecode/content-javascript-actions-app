74e67ed1fcf8ebb008259c65235b9c401b4592a8
alexawesomecode <alexawesomecode@gmail.com>
2020-11-16 12:53:02 -0300

    v2.3

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-} catch (error) {
+} catch (error) { console.log('hello')}
